1b61a33125d0072e83a642c70b2d905bb1894e7f
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-05-26 18:58:09 +0900

    override bobthefish settings

--- config.fish
+++ config.fish
@@ -1,1 +1,9 @@
 source ~/.config/fish/chpwd.fish
+
+# oh-my-fish/theme-bobthefish
+set -g theme_display_git_master_branch yes
+set -g theme_display_date no
+set -g theme_color_scheme nord
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt '$ '
