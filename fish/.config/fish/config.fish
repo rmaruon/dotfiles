2c8440fa3c553e13cfdd037d8a2aee3aabf6a5f5
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-05-28 18:56:29 +0900

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,6 +6,6 @@ source ~/.config/fish/env.fish
 set -g theme_display_git_master_branch yes
 set -g theme_display_date no
 set -g theme_color_scheme nord
-set -g fish_prompt_pwd_dir_length 0
+set -g fish_prompt_pwd_dir_length 1
 set -g theme_newline_cursor yes
 set -g theme_newline_prompt '$ '
