6a603951f9cb8fae9b3e0acc3d894234df0addf9
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-11-07 22:41:34 +0900

    update env.fish

--- fish/.config/fish/env.fish
+++ fish/.config/fish/env.fish
@@ -2,6 +2,9 @@
 set -x N_PREFIX $HOME/.n
 set -x PATH $PATH $N_PREFIX/bin
 
+# node
+set PATH ./node_modules/.bin $PATH
+
 # python
 set -x PIPENV_VENV_IN_PROJECT true
 set -x PYTHONIOENCODING utf-8
