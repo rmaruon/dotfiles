cfc4ebc9f11d1169c1d5eb2da8a160de604acb30
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-11-08 17:41:25 +0900

    replace aliase with abbr

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -1,12 +1,12 @@
-alias c="code"
-alias C="pbcopy"
-alias cl="clear"
-alias e="explorer.exe"
-alias f='open -a Finder ./'
-alias g="git"
-alias o="open"
-alias t="tig"
-alias where="which -a"
+abbr c "code"
+abbr C "pbcopy"
+abbr cl "clear"
+abbr e "explorer.exe"
+abbr f 'open -a Finder ./'
+abbr g "git"
+abbr o "open"
+abbr t "tig"
+abbr where "which -a"
 
 function mkcd
     mkdir $argv
