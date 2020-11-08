ae902d18de40b949e7565f9a8e677931920ba9ee
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-11-08 19:43:20 +0900

    fish: add git abbr

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -6,10 +6,22 @@ abbr C "pbcopy"
 abbr cl "clear"
 abbr e "explorer.exe"
 abbr f 'open -a Finder ./'
-abbr g "git"
 abbr o "open"
 abbr t "tig"
 abbr where "which -a"
+
+# git
+abbr g    "git"
+abbr gb   "git branch --all"
+abbr gbd  "git branch -d"
+abbr gcam "git commit --amend --no-edit"
+abbr gcob "git checkout -b"
+abbr gf   "git fetch"
+abbr gpl  "git pull"
+abbr gps  "git push"
+abbr gs   "git status --short"
+abbr gsm  "git switch master"
+abbr gst  "git stash"
 
 function mkcd
     mkdir $argv
